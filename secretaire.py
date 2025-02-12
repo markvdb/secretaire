2d062dd6e1700414e05db80d72a6100006303947
Mark Van den Borre <mark@fosdem.org>
2025-02-12 12:47:53 +0100

    add some pseudocode

--- secretaire.py
+++ secretaire.py
@@ -16,12 +16,12 @@ def switch_tty(tty_number):
 
 
 def select_task():
-    options = ['calendar','contacts', 'email', 'news', 'radio', 'sms', 'wikipedia', 'youtube']
+    options = ['calendar','call', 'contacts', 'email', 'rss', 'radio', 'record', 'sms', 'stocks', 'wikipedia', 'youtube']
     task = ''
     task_menu = "Make your choice:\n"
     for index, item in enumerate(options):
         task_menu += f'{index+1}) {item}\n'
-    while task.lower() not in options:
+    while task not in options:
         task = input(task_menu)
     return (task)
 
@@ -34,6 +34,109 @@ while True:
         i = input('subject: ')
         if (i == 'q'): break
         match task:
+            case 'calendar':
+                print('TODO implenent')
+                '''
+                '''
+
+            case 'call':
+                print('TODO implement')
+                '''
+                read name
+                find first phone number for name
+                pick up 1980 phone
+                voip system calls retrieved phone number
+                '''
+            
+            case 'contacts':
+                print('TODO implement')
+                '''
+                '''
+
+            case 'email':
+                print('TODO implement')
+                '''
+                subswitch:
+                    - print overview of new emails
+                    - print individual email
+                        - enter email id
+                    - mark as read:
+                        - enter email id
+                    - mark as spam
+                        - enter email id
+                    - delete
+                        - enter email id
+                    - reply
+                        - enter email id
+                        - either
+                            - write answer on paper by hand; scan
+                            - write anwer on paper by typewriter; scan
+                            - type blind on keyboard
+                    - reply all
+                        - enter email id
+                    - forward
+                        - enter email id
+                '''
+
+            case 'rss':
+                print('TODO implement')
+                '''
+                set rotary switch to feed
+                print feed
+                '''
+
+            case 'radio':
+                print('TODO implement')
+                '''
+                set subswitch to radio station or podcast
+                how to set radio station or podcast?
+                  - web interface outside?
+                  - keyboard + search service?
+                '''
+
+            case 'record':
+                print('TODO implement')
+                '''
+                - pick up phone
+                - speak
+                - put down phone horn
+                - recording stops
+                '''
+
+            case 'sms':
+                print('TODO implement')
+                '''
+                subswitch
+                    - show sms (print? split flap?)
+                    - write sms
+                        - read name
+                        - find first mobile phone number for name
+                        - write sms
+                            - keyboard?
+                            - paper & scan?
+                        - connect to own mobile phone via bluetooth
+                        - send sms
+                        - feedback
+                            - print sent sms
+                            - store in sms archive
+                '''
+
+            case 'stocks':
+                print('TODO implement')
+                '''
+                subswitch
+                    - print portfolio statement
+                    - net worth (split flap, galvanometer?)
+                    - individual stock 1: shortcode, price, evolution (split flap, galvanometer?)
+                '''
+            case 'wikipedia':
+                print('TODO implement')
+                '''
+                - enter search string on keyboard
+                - print first article matching search string
+                '''
+                searchstring = 'https://en.wikipedia.org/w/api.php?action=opensearch&search={i}&limit=1&namespace=0&format=json'
+
             case 'youtube':
                 searchstring = 'ytdl://ytsearch100:\''+ i + '\''
                 subprocess.run(['mpv', '--no-video', searchstring])
